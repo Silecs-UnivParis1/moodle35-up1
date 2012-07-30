5213651b0fc9150904e17e0626e64dce83969705
François Gannaz <francois.gannaz@silecs.info>
2012-07-30 21:55:26 +0200

    SOAP works

--- local/rof_sync/soaptest.php
+++ local/rof_sync/soaptest.php
@@ -5,10 +5,9 @@ $urlext ='cdm/services/cataManager';
 
 
 $url = $urlbase . $urlext . '?wsdl' ;
-$soapClient = new SoapClient($url);
+$soapClient = new SoapClient($url, array('trace' => 1));
 
-//print_r($soapClient);
-
+/*
 echo "Functions: ";
 $functions = $soapClient->__getFunctions();
 print_r($functions);
@@ -18,57 +17,33 @@ $types = $soapClient->__getTypes();
 print_r($types);
 
 echo "\n\n**************\n\n";
-
-//list of formations
-$url = $urlbase . $urlext . '?wsdl' ;
-$soapClient = new SoapClient($url, array('trace' => 1));
-
-/*
-$requestParams = array(
-    '_cmd' => 'getAllFormations',
-    '_lang' => 'fr-FR',
-    );
 */
+
 $reqParams = array(
     '_cmd' => 'getFormation',
     '_lang' => 'fr-FR',
     '_oid' => 'UP1-PROG34252',
-    );
+);
 
-$n = array(); $v = array(); $i=0;
+$v = array();
 foreach($reqParams as $key=>$value) {
-    $n[$i] = $key;
-    $v[$i] = array($value);
-    $i++;
+    $v[] = array($value);
 }
 $callParams = array(
-    'names' => $n,
+    'names' => array_keys($reqParams),
     'values' => $v,
-    );
-print_r($callParams);
-
+);
 
 try {
-    $formResponse = $soapClient->__soapCall('getResponse', $reqParams);
-    // $formResponse = $soapClient->__soapCall('getResponse', $callParams);
+    //$formResponse = $soapClient->getResponse($reqParams);
+    $formResponse = $soapClient->getResponse($callParams, '1010');
     // $formResponse = $soapClient->__soapCall('getResponse', $reqParams);
+    echo $formResponse;
 } catch (SoapFault $soapFault) {
     echo "SoapFault : \n";
     echo $soapFault;
     echo "\nFin SoapFault\n\n" ;
 }
 
-$handle = fopen("lastrequest.xml", "w");
-fwrite ($handle, $soapClient->__getLastRequest());
-// echo "Request : \n", $soapClient->__getLastRequest(), "\n";
-fclose($handle);
-$handle = fopen("lastresponse.xml", "w");
-fwrite ($handle, $soapClient->__getLastResponse());
-// echo "Response : \n", $soapClient->__getLastResponse(), "\n";
-fclose($handle);
-
-
-// $formResponse = $soapClient->getResponse($requestParams);
-
-
-//var_dump($formResponse);
+file_put_contents("lastrequest.xml", $soapClient->__getLastRequest());
+file_put_contents("lastresponse.xml", $soapClient->__getLastResponse());
