934551a4e2f1a00ba46910e74f8f33fa32cd2571
Guillaume Allegre <guillaume.allegre@silecs.info>
2012-08-18 15:52:49 +0200

    rof: fetch constants

---
+++ local/rof_sync/locallib.php
@@ -0,0 +1,128 @@
+<?php
+
+define('CLI_SCRIPT', true);
+require(dirname(dirname(dirname(__FILE__))).'/config.php'); // global moodle config file.
+
+$rofUrl = 'http://formation.univ-paris1.fr/cdm/services/cataManager?wsdl' ;
+
+// fetchConstants();
+
+return 0;
+
+
+/**
+ * fetch "constantes" from webservice and insert them into table rof_constant
+ *
+ * @return lastinsertid
+ * @todo manage updates ?
+ */
+function fetchConstants() {
+global $DB;
+
+    $reqParams = array(
+        '_cmd' => 'getAllFormations',
+        '_lang' => 'fr-FR',
+        '__composante' => '00', // fausse composante, pour obtenir uniquement les constantes
+        '__1' => '__composante',  // incompréhensible mais nécessaire
+    );
+    $xmlResp = doSoapRequest($reqParams);
+    $xmlTree = new SimpleXMLElement($xmlResp);
+    $constants = $xmlTree->properties->infoBlock->extension->uniform->constantes;
+
+    // step 1 : element domaineDiplome
+    foreach ($constants->domaineDiplome as $dd) {
+        $elt='domaineDiplome';
+        $elttype = (string)$dd->attributes()->type;
+        foreach ($dd->data as $singledata) {
+            $record = new stdClass();
+            $record->element = $elt;
+            $record->elementtype = $elttype;
+            $record->dataid = (string)$singledata->attributes()->id;
+            $record->dataimport = (string)$singledata->attributes()->import;
+            $record->dataoai = (string)$singledata->attributes()->oai;
+            $record->value = (string)$singledata->value;
+            // print_r($record);
+            $lastinsertid = $DB->insert_record('rof_constant', $record);
+        }
+    }
+    // step 2 : other elements
+    foreach ($constants->children() as $element) {
+        $elt = (string)$element->getName();
+        if ($elt == 'domaineDiplome') continue;
+        foreach ($element->data as $singledata) {
+            $record = new stdClass();
+            $record->element = $elt;
+            $record->elementtype = '';
+            $record->dataid = (string)$singledata->attributes()->id;
+            $record->dataimport = (string)$singledata->attributes()->import;
+            $record->dataoai = (string)$singledata->attributes()->oai;
+            $record->value = (string)$singledata->value;
+            // print_r($record);
+            $lastinsertid = $DB->insert_record('rof_constant', $record);
+        }
+    }
+    return $lastinsertid;
+}
+
+
+/**
+ * turns "logical" parameters into the form needed by the webservice
+ * @param type $reqParams array of parameters
+ * @return string XML response
+ */
+function doSoapRequest($reqParams) {
+    global $rofUrl;
+
+    $callParams = setCallParams($reqParams);
+    $soapClient = new SoapClient($rofUrl, array('trace' => 1));
+
+    try {
+        $formResponse = $soapClient->getResponse($callParams, '1010');
+        return $formResponse;
+    } catch (SoapFault $soapFault) {
+        echo "SoapFault : \n";
+        echo $soapFault;
+        file_put_contents("lastrequest.xml", $soapClient->__getLastRequest());
+        echo "\nFin SoapFault\n\n" ;
+        return false;
+    }
+}
+
+
+
+/**
+ * Turn "logical" $reqParams into "artificial" $callParams used to request
+ * the CDM-fr web service
+ * @param  array $reqParams
+ * @return $callParams
+ */
+function setCallParams($reqParams) {
+    $v = array();
+    foreach($reqParams as $key=>$value) {
+        $v[] = array($value);
+    }
+    $callParams = array(
+        'names' => array_keys($reqParams),
+        'values' => $v,
+    );
+    return $callParams;
+}
+
+
+/**
+ * Display the WSDL auto-documented prototypes
+ * @return void
+ */
+function displayWsdlInformation($url) {
+    $soapClient = new SoapClient($url);
+
+    echo "Functions: ";
+    $functions = $soapClient->__getFunctions();
+    print_r($functions);
+
+    echo "\nTypes: ";
+    $types = $soapClient->__getTypes();
+    print_r($types);
+
+    echo "\n\n**************\n\n";
+}
