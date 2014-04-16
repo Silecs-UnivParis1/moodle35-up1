3aa2bff44e2cc3ad84c85dd93ec5ae8016fab2c7
Cécile Guilloux <cecile.guilloux@silecs.info>
2014-04-16 11:38:13 +0200

    Mise à jour 2.4 -> 2.6 : plugin /local/up1_notificationcourse  - M#1225

--- local/up1_notificationcourse/lib_notificationcourse.php
+++ local/up1_notificationcourse/lib_notificationcourse.php
@@ -6,114 +6,107 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-function get_notificationcourse_message($formdata, $params) {
+/**
+ * construit l'objet $message contenant le sujet et le corps de message version texte et html
+ * @param string $subject
+ * @return object $message
+ */
+function get_notificationcourse_message($subject, $msgbodyinfo, $complement) {
     $message = new object();
-    $sitename = '[' . format_string(get_site()->shortname) . '] ';
-    $message->subject = $formdata->msgsubject;
-    $message->body = $formdata->msgbody;
+    $message->subject = $subject;
+    $message->from = $msgbodyinfo['shortnamesite'];
+    $comhtml = '';
+    $comtext = '';
+    if (trim($complement) !='') {
+        $comhtml .= '<p>' . $complement . '</p>';
+        $comtext .= "\n\n" . $complement;
+    }
+    $message->bodyhtml = '<p>' . get_email_body($msgbodyinfo, 'html') . '</p>' . $comhtml;
+    $message->bodytext = get_email_body($msgbodyinfo, 'text') . $comtext;
 
-    $message->subject = $sitename . $message->subject;
-    //interpolation variables si besoin
-    $message->subject = str_replace('[[nom_activite]]', $params['nom_activite'], $message->subject);
-    $message->body = str_replace('[[lien_activite]]', $params['lien_activite'], $message->body);
-
+    $message->bodyhtml .= '<p>' . $msgbodyinfo['coursepath']
+        . '<br/><a href="' . $msgbodyinfo['urlactivite']
+        . '">' . $msgbodyinfo['urlactivite'] . '</a></p>';
+    $message->bodytext .= "\n\n" . $msgbodyinfo['coursepath']
+        . "\n" . $msgbodyinfo['urlactivite'];
     return $message;
 }
 
 /**
- * renvoie les identifiants des utilisateurs ayant le rôle 'rolename'
+ * construit le messsage d'interface du nombre et de la qualité des
+ * destinataires du message
+ * @param int $nbdest
+ * @param int $groupingid
+ * @param array $msgbodyinfo
+ * @return string $label
+ */
+function get_label_destinataire($nbdest, $groupingid, $msgbodyinfo) {
+    $label = '';
+    if ($nbdest == 0) {
+        return get_string('norecipient', 'local_up1_notificationcourse');
+    }
+    $x = 'à l\'';
+    $s = '';
+    if ($nbdest > 1) {
+        $x = 'aux ' . $nbdest . ' ';
+        $s = 's';
+    }
+    $label = 'Le message suivant sera transmis ' . $x
+        . 'utilisateur' . $s;
+    if ($groupingid == 0) {
+        $label .= ' inscrit' . $s . ' à cet espace.';
+    } else {
+        $label .= ' concerné' . $s . ' par <a href="' . $msgbodyinfo['urlactivite']
+            . '">' . $msgbodyinfo['nomactivite'] . '.</a>';
+    }
+    return $label;
+}
+
+/**
+ * renvoie les utilisateurs ayant le rôle 'rolename'
  * dans le cours $course
  * @param object $course $course
  * @param string $rolename shortname du rôle
- * @return array iduser => iduser
+ * @return array de $user
  */
 function get_users_from_course($course, $rolename) {
     global $DB;
     $coursecontext = context_course::instance($course->id);
-
     $rolestudent = $DB->get_record('role', array('shortname'=> $rolename));
+    $studentcontext = get_users_from_role_on_context($rolestudent, $coursecontext);
 
-    $students = get_users_from_role_on_context($rolestudent, $coursecontext);
-
-    $mystudents = array();
-
-    if (isset($students) && count($students)) {
-        foreach ($students as $student) {
-            $mystudents[$student->userid] = $student->userid;
-        }
+    if ($studentcontext == 0) {
+        return $studentcontext;
     }
-    return $mystudents;
+    $ids = '';
+    foreach ($studentcontext as $sc) {
+        $ids .= $sc->userid . ',';
+    }
+    $ids = substr($ids, 0, -1);
+    $sql = "SELECT id, firstname, lastname, email, mailformat FROM {user} WHERE id IN ({$ids})";
+    $students = $DB->get_records_sql($sql);
+
+    return $students;
 }
 
-
-
 /**
- * Envoi une notification au user
+ * Envoi une notification aux $users + copie à $USER
  * @param array $idusers
  * @param object $msg
  * @param array $infolog informations pour le log pour les envois de mails
  * @return string : message interface
  */
-function send_notificationcourse($idusers, $msg, $infolog) {
-    global $DB;
-    $ids = '';
-    if ($idusers && count($idusers)) {
-        foreach ($idusers as $id) {
-            $ids .= $id . ',';
-        }
-        $ids = substr($ids, 0, -1);
-    }
-    return notificationcourse_send_all_message($ids, $msg, $infolog);
-}
-
-/**
- * Envoi une notification au user dont l'identifiant apparait dans $ids
- * @param string $ids : liste des identifiants user (format : id1,id2,id3
- * @param object $msg
- * @param array $infolog informations pour le log pour les envois de mails
- * @return string : message interface
- */
-function notificationcourse_send_all_message($ids, $msg, $infolog) {
-    global $DB, $USER;
+function send_notificationcourse($users, $msg, $infolog) {
+    global $USER;
     $nb = 0;
-    if ($ids != '') {
-        $sql = "SELECT id, firstname, lastname, email FROM {user} WHERE id IN ({$ids})";
-        $users = $DB->get_records_sql($sql);
-
-        foreach ($users as $user) {
-            $res = notificationcourse_send_email($user->email, $msg->subject, $msg->body);
-            if ($res) {
-                ++$nb;
-            }
+    foreach ($users as $user) {
+        $res = notificationcourse_send_email($user, $msg);
+        if ($res) {
+            ++$nb;
         }
-        /** pour messagerie interne cf http://docs.moodle.org/dev/Messaging_2.0#Message_dispatching
-        $userfrom = new object();
-        static $supportuser = null;
-        if (!empty($supportuser)) {
-            $userfrom = $supportuser;
-        } else {
-            $userfrom = $USER;
-        }
-        $eventdata = new object();
-        $eventdata->component = 'moodle';
-        $eventdata->name = 'courserequested';
-        $eventdata->userfrom = $userfrom;
-        $eventdata->subject = $msg->subject; //** @todo get_string()
-        $eventdata->fullmessageformat = FORMAT_PLAIN;   // text format
-        $eventdata->fullmessage = $msg->body;
-        $eventdata->fullmessagehtml = '';   //$messagehtml;
-        $eventdata->smallmessage = $msg->body; // USED BY DEFAULT !
-        foreach ($users as $user) {
-            $eventdata->userto = $user;
-            $res = message_send($eventdata);
-            if ($res) {
-                ++$nb;
-            }
-        }
-        **/
     }
+    notificationcourse_send_email($USER, $msg);
     $infolog['nb'] = $nb;
-
     return get_result_action_notificationcourse($infolog);
 }
 
@@ -123,36 +116,84 @@ function notificationcourse_send_all_message($ids, $msg, $infolog) {
  * @return string message interface
  */
 function get_result_action_notificationcourse($infolog) {
-    $s = '';
-
     if ($infolog['nb'] == 0) {
-        return get_string('nopostssend', 'local_up1_notificationcourse');
-    } elseif ($infolog['nb'] > 1) {
-        $s = 's';
+        return get_string('nomessagesend', 'local_up1_notificationcourse');
     }
-
-    $message = $infolog['nb'] . ' notification' . $s . ' envoyée' . $s;
+    $message = get_string('numbernotification', 'local_up1_notificationcourse', $infolog['nb']);
     //log
     add_to_log($infolog['courseid'], 'up1_notif_course', 'send notification_course',
         $infolog['cmurl'], $message , $infolog['cmid'], $infolog['userid']);
-
     return $message;
 }
 
 /**
  * Envoie un email à l'adresse mail spécifiée
  * @param string $email
- * @param string $subject,
- * @param string $message
+ * @param object $msg
  * @return false ou resultat de la fonction email_to_user()
  **/
-function notificationcourse_send_email($email, $subject, $message) {
-    if (!isset($email) && empty($email)) {
+function notificationcourse_send_email($user, $msg) {
+    if (!isset($user->email) && empty($user->email)) {
         return false;
     }
-    $supportuser = core_user::get_support_user();
-    $user = new stdClass();
-    $user->email = $email;
-    return email_to_user($user, $supportuser, $subject, $message);
+    $emailform = $msg->from;
+    return email_to_user($user, $emailform, $msg->subject, $msg->bodytext, $msg->bodyhtml);
+}
+
+/**
+ * construit le sujet du mail envoyé
+ * @param string $siteshortname
+ * @param string $courseshortname
+ * @param string $activitename
+ * @return string
+ */
+function get_email_subject($siteshortname, $courseshortname, $activitename) {
+    $subject = '';
+    $subject .='['. $siteshortname . '] '. get_string('notification', 'local_up1_notificationcourse')
+        . $courseshortname . ' - ' . $activitename;
+    return $subject;
+}
+
+/**
+ * construit le
+ * @param array $msgbodyinfo
+ * @param string $type
+ * return string
+ */
+function get_email_body($msgbodyinfo, $type) {
+    $res = '';
+    $coursename = $msgbodyinfo['shortnamecourse'] . ' - ' . $msgbodyinfo['fullnamecourse'];
+    $a = new stdClass();
+    $a->sender = $msgbodyinfo['user'];
+    $a->linkactivity = $msgbodyinfo['nomactivite'];
+    $a->linkcourse = $msgbodyinfo['shortnamecourse'] . ' - ' . $msgbodyinfo['fullnamecourse'];
+    if ($type == 'html') {
+        $a->linkactivity = html_Writer::link($msgbodyinfo['urlactivite'], $msgbodyinfo['nomactivite']);
+        $a->linkcourse = html_Writer::link($msgbodyinfo['urlcourse'], $coursename);
+    }
+    $res .= get_string('msgsender', 'local_up1_notificationcourse', $a);
+    return $res;
+}
+
+/**
+ * Construit le chemin categories > cours
+ * @param array $categories tableau de tableaux
+ * @param object $course
+ * @return string $path
+ */
+function get_pathcategories_course($categories, $course) {
+    $path ='';
+    $tabcat = array();
+    if (count($categories)) {
+        foreach ($categories as $category) {
+            $tabcat[$category->depth] = $category->name;
+        }
+        ksort($tabcat);
+        foreach ($tabcat as $cat) {
+            $path .= $cat . ' > ';
+        }
+    }
+    $path .= $course->shortname;
+    return $path;
 }
 ?>
