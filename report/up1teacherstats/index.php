6fe4fe40fc259b61ac306c6c0c0e243c12421410
Guillaume Allegre <guillaume.allegre@silecs.info>
2014-05-31 17:08:46 +0200

    report_up1teacherstats draft

---
+++ report/up1teacherstats/index.php
@@ -0,0 +1,104 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Display course up1teacherstats page
+ *
+ * @package    report
+ * @subpackage up1teacherstats
+ * @copyright  2012-2014 Silecs {@link http://www.silecs.info}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * derived from package report_outline
+ */
+
+require('../../config.php');
+require_once(__DIR__ . '/locallib.php');
+
+global $DB, $PAGE, $OUTPUT;
+ /* @var $PAGE moodle_page */
+
+$id = required_param('id', PARAM_INT);       // course id
+$layout = optional_param('layout', 'report', PARAM_ALPHA); // default layout=report
+if ($layout != 'popup') {
+    $layout = 'report';
+}
+
+$course = $DB->get_record('course', array('id' => $id), '*', MUST_EXIST);
+$PAGE->set_course($course);
+
+$PAGE->set_url('/report/up1teacherstats/index.php', array('id'=>$id));
+$PAGE->set_pagelayout($layout);
+$PAGE->requires->css(new moodle_url('/report/up1teacherstats/styles.css'));
+
+$site = get_site();
+$strreport = get_string('pluginname', 'report_up1teacherstats');
+$pagename = up1_meta_get_text($course->id, 'up1nomnorme', false);
+if ( ! $pagename ) {
+    $pagename = $course->fullname;
+}
+
+$PAGE->set_title($pagename); // $course->shortname .': '. $strreport); // tab title
+$PAGE->set_heading($site->fullname);
+echo $OUTPUT->header();
+
+echo "<h2>" . $pagename . "</h2>\n";
+
+echo '<div id="teacherstats-bigbutton">' . "\n";
+html_button_join($course);
+if ( has_capability('local/crswizard:supervalidator', context_system::instance()) )
+{
+    $urlboard = new moodle_url('/local/courseboard/view.php', array('id' => $course->id));
+    $icon = $OUTPUT->action_icon($urlboard, new pix_icon('i/settings', 'Afficher le tableau de bord'));
+    echo $icon;
+}
+echo '</div>' . "\n";
+
+echo "<h3>Utilisateurs inscrits dans l’EPI</h3>\n";
+//html_table_stats_enrolments($course);
+
+
+
+echo "<h3>Fréquentation</h3>\n";
+
+echo "<h4>Pour le mois écoulé</h4>\n";
+//html_table_stats_daily_month($course);
+
+echo "<h4>Depuis l’ouverture</h4>\n";
+//html_table_stats_weekly_opening($course);
+
+
+
+echo "<h3>Ressources les plus consultées</h3>\n";
+//html_table_stats_resources_top($course);
+$table = new html_table();
+$table->head = array('Rang', 'Titre', 'Type', 'Nombre d’affichages');
+$table->data = teacherstats_resources_top($course->id, 10);
+echo html_writer::table($table);
+
+
+echo "<h3>Activités en cours</h3>\n";
+
+echo "<h4>Devoirs</h4>\n";
+//html_table_assignments($course);
+
+echo "<h4>Glossaire, base de données, forum, wiki, chat</h4>\n";
+//html_table_activities($course);
+
+echo "<h4>Quizz, Sondage, Feedback</h4>\n";
+//html_table_quizzes($course);
+
+
+echo $OUTPUT->footer();
