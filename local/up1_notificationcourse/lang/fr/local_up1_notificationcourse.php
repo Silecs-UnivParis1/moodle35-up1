4b04028fcde442fec7a2fbce7a720c699b23d29e
Cécile Guilloux <cecile.guilloux@silecs.info>
2014-06-10 12:18:03 +0200

    up1_notificationcourse - corrections fichiers langues

--- local/up1_notificationcourse/lang/fr/local_up1_notificationcourse.php
+++ local/up1_notificationcourse/lang/fr/local_up1_notificationcourse.php
@@ -6,17 +6,19 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+$string['pluginname'] = 'Notification activité';
+
 $string['body'] = 'Corps du message : ';
 $string['complement'] = 'Complément de message : ';
-$string['msgsender'] = '{$a->sender} souhaite attirer votre attention sur l\'élément '
-    . '{$a->linkactivity} proposé au sein de l\'espace {$a->linkcourse}.';
 $string['nomessagesend'] = 'Aucun message envoyé.';
 $string['norecipient'] = 'Aucun destinataire';
-$string['notification'] = 'Notification : ';
 $string['numbernotification'] = 'Nombre de notifications envoyées : {$a}';
-$string['pluginname'] = 'Notification activité';
 $string['returncourse'] = 'Retour au cours';
 $string['sender'] = 'Expéditeur : ';
 $string['sendnotification'] = 'Envoyer une notification';
 $string['subject'] = 'Sujet : ';
 $string['submit'] = 'Envoyer';
+
+// The following strings are part of the sent message
+$string['notification'] = 'Notification : ';
+$string['msgsender'] = '{$a->sender} souhaite attirer votre attention sur l\'élément {$a->linkactivity} proposé au sein de l\'espace {$a->linkcourse}.';
