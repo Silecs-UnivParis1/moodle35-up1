8a8867f1932f10ab6cac268374304dd783a82441
Guillaume Allegre <guillaume.allegre@silecs.info>
2014-07-21 08:12:46 +0200

    specifications url

--- local/coursehybridtree/CourseHybridTree.php
+++ local/coursehybridtree/CourseHybridTree.php
@@ -1,4 +1,12 @@
 <?php
+
+/*
+ * This class (and descendants) purpose is to implement the specifications given at
+ * http://paris1-dev.silecs.info/wiki/doku.php/catalogue_des_cours:arbre_des_cours?&#consignes
+ * and commented on http://tickets.silecs.info/mantis/view.php?id=2163
+ */
+
+
 class CourseHybridTree
 {
 
