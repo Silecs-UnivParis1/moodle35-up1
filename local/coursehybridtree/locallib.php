712ccab76d5074044f046026c947d1eb93ffd2d6
Guillaume Allegre <guillaume.allegre@silecs.info>
2014-07-24 20:42:10 +0200

    Cht locallib.php typo

--- local/coursehybridtree/locallib.php
+++ local/coursehybridtree/locallib.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../../config.php';
 require __DIR__ . '/CourseHybridTree.php';
+require __DIR__ . '/ChtNode.php';
 require __DIR__ . '/ChtNodeCategory.php';
 require __DIR__ . '/ChtNodeRof.php';
 require __DIR__ . '/ChtNodeCourse.php';
