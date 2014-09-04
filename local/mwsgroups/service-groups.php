e2740eb96ee140b4842252a098d453a004e2baea
François Gannaz <francois.gannaz@silecs.info>
2014-09-04 20:01:47 +0200

    mwsgroups: use the class

--- local/mwsgroups/service-groups.php
+++ local/mwsgroups/service-groups.php
@@ -7,25 +7,22 @@ require_once(__DIR__ . '/lib.php');
 $token = required_param('token', PARAM_RAW);
 $maxrows = optional_param('maxRows', 0, PARAM_INT);
 $maxrowsfor = array(
-    'users' => optional_param('userMaxRows', 0, PARAM_INT),
-    'groups' => optional_param('groupMaxRows', 0, PARAM_INT),
+    'users' => optional_param('userMaxRows', $maxrows, PARAM_INT),
+    'groups' => optional_param('groupMaxRows', $maxrows, PARAM_INT),
 );
 $filterstudent = optional_param('filter_student', 'both', PARAM_ALPHA);
 $filtergroupcat = optional_param('filter_group_category', '', PARAM_ALPHANUMEXT);
 $callback = optional_param('callback', '', PARAM_ALPHANUMEXT); // if set, use jsonp instead of json
 
-if (!$maxrows || $maxrows > MWS_SEARCH_MAXROWS) {
-    $maxrows = MWS_SEARCH_MAXROWS;
-}
-foreach ($maxrowsfor as $type => $max) {
-    if (!$max || $max > MWS_SEARCH_MAXROWS) {
-        $maxrowsfor[$type] = $maxrows;
-    }
-}
-
 $PAGE->set_context(context_system::instance());
 
-$res = mws_search($token, $maxrowsfor['users'], $maxrowsfor['groups'], $filterstudent, $filtergroupcat);
+$u_g = new mws_search_groups();
+$u_g->token = $token;
+$u_g->usermaxrows = $maxrowsfor['users'];
+$u_g->groupmaxrows = $maxrowsfor['groups'];
+$u_g->filterstudent = $filterstudent;
+$u_g->filtergroupcat = $filtergroupcat;
+$res = $u_g->search();
 
 if (empty($callback)) {
     header('Content-Type: application/json; charset="UTF-8"');
