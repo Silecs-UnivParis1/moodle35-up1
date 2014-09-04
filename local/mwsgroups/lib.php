cdf44ceb43ba04a18b63018678b952f81d6176f6
François Gannaz <francois.gannaz@silecs.info>
2014-09-04 19:54:50 +0200

    mwsgroups: the main function is now a wrapper on a class

--- local/mwsgroups/lib.php
+++ local/mwsgroups/lib.php
@@ -14,8 +14,143 @@ define('MWS_SEARCH_MAXROWS', 100);
 
 require __DIR__ . '/lib_users.php';
 
+class mws_search_groups
+{
+    /**
+     * @var string to search in user and cohort tables
+     */
+    public $token;
+
+    /**
+     * @var int
+     */
+    public $usermaxrows;
+
+    /**
+     * @var int
+     */
+    public $groupmaxrows = MWS_SEARCH_MAXROWS;
+
+    /**
+     * @var string 'no' | 'only' | 'both' (default)
+     */
+    public $filterstudent = 'both';
+
+    /**
+     * @var string '' (default) | 'structures' | 'affiliation' | ...
+     */
+    public $filtergroupcat = '';
+
+    /**
+     * Emulates wsgroups "search" action from Moodle data (???)
+     *
+     * @return array array('users' => $users, 'groups' => $groups)
+     */
+    public function search() {
+        if ($this->groupmaxrows > MWS_SEARCH_MAXROWS || $this->groupmaxrows == 0) {
+            $this->groupmaxrows = MWS_SEARCH_MAXROWS;
+        }
+        if ($this->usermaxrows > MWS_SEARCH_MAXROWS || $this->usermaxrows == 0) {
+            $this->usermaxrows = MWS_SEARCH_MAXROWS;
+        }
+
+        $search_u = new mws_search_users();
+        $search_u->maxrows = $this->usermaxrows;
+        $search_u->filterstudent = $this->filterstudent;
+        $users  = $search_u->search($this->token);
+
+        if ($this->filtergroupcat == '') {
+            $groups = $this->search_groups();
+        } else {
+            $groups = $this->search_groups_category($this->filtergroupcat);
+        }
+        return array('users' => $users, 'groups' => $groups);
+    }
+
+    /**
+     * search groups according to filters
+     * @return array
+     */
+    protected function search_groups() {
+        $wherecat = self::categoryToWhere();
+
+        $res = array();
+        foreach (array_keys($wherecat) as $cat) {
+            // echo "<b> $cat -> $where : " . count($groups) . " results</b><br />\n" ; //DEBUG
+            $res = array_merge($res, $this->search_groups_category($cat));
+        }
+        return $res;
+    }
+
+    /**
+     * search groups according to filters
+     * @global type $DB
+     * @param string $category Group/cohort category, see below
+     * @return array
+     */
+    protected function search_groups_category($category) {
+        global $DB;
+        $ptoken = '%' . $DB->sql_like_escape($this->token) . '%';
+
+        $wherecat = self::categoryToWhere();
+        $cterms = explode('|', $category);
+        $cwhere = array();
+        foreach ($cterms as $term) {
+            if (isset($wherecat[$term])) {
+                $cwhere[] = $wherecat[$term];
+            }
+        }
+        if (!$cwhere) {
+            return array();
+        }
+        $sql = "SELECT id, name, idnumber, description, descriptionformat FROM {cohort} WHERE "
+            . "( name LIKE ? OR idnumber LIKE ? ) AND (" . join(' OR ', $cwhere) . ')' ;
+        // echo $sql . " <br />\n" ; //DEBUG
+        $records = $DB->get_records_sql($sql, array($ptoken, $ptoken), 0, $this->groupmaxrows);
+        $groups = array();
+        $order = 0;
+        foreach ($records as $record) {
+            $order++;
+            $size = $DB->count_records('cohort_members', array('cohortid' => $record->id));
+            $groups[] = array(
+                'key' => $record->idnumber,
+                'name' => $record->name,
+                'description' => format_text($record->description, $record->descriptionformat),
+                'category' => groupKeyToCategory($record->idnumber),
+                'size' => $size,
+                'order' => $order
+            );
+        }
+        return $groups;
+    }
+
+    /**
+     * sort of reciprocal from groupKeyToCategory
+     * return array assoc. array of WHERE conditions in the SQL syntax
+     */
+    protected static function categoryToWhere() {
+        $patterns = array(
+            'structures' => 'structures-%',
+            'affiliation' => 'affiliation-%',
+            'diploma' => 'diploma-%',
+            'gpelp' => 'groups-gpelp.%',
+            //'gpetp' => 'groups-gpetp.%',
+            'elp' => 'groups-mati%'
+        );
+        $res = array();
+        $other = '';
+        foreach ($patterns as $cat => $pattern) {
+            $res[$cat] = "idnumber LIKE '$pattern' ";
+            $other = $other . "idnumber NOT LIKE '$pattern' AND ";
+        }
+        $res['other'] = substr($other, 0, -4); //drop the last AND
+        return $res;
+    }
+}
+
 /**
- * emulates wsgroups "search" action from Moodle data
+ * Wrapper on the class mws_search_groups, emulates wsgroups "search" action from Moodle data
+ *
  * @param string $token to search in user and cohort tables
  * @param int $usermaxrows
  * @param int $groupmaxrows
@@ -24,81 +159,14 @@ require __DIR__ . '/lib_users.php';
  * @return array('users' => $users, 'groups' => $groups)
  */
 function mws_search($token, $usermaxrows, $groupmaxrows, $filterstudent='both', $filtergroupcat='') {
-    $search_u = new mws_search_users();
-    $search_u->maxrows = $usermaxrows;
-    $search_u->filterstudent = $filterstudent;
-    $users  = $search_u->search($token);
-
-    if ($filtergroupcat == '') {
-        $groups = mws_search_groups($token, $groupmaxrows);
-    } else {
-        $groups = mws_search_groups_category($token, $filtergroupcat, $groupmaxrows);
-    }
-    return array('users' => $users, 'groups' => $groups);
+    $s = new mws_search_groups();
+    $s->token = $token;
+    $s->usermaxrows = $usermaxrows;
+    $s->groupmaxrows = $groupmaxrows;
+    $s->filterstudent = $filterstudent;
+    $s->filtergroupcat = filtergroupcat;
+    return $s->search();
 }
-
-/**
- * search groups according to filters
- * @param string $token to search in cohort table
- * @param int $maxrows
- * @return array
- */
-function mws_search_groups($token, $maxrows) {
-    $wherecat = categoryToWhere();
-
-    $res = array();
-    foreach (array_keys($wherecat) as $cat) {
-        $groups = mws_search_groups_category($token, $cat, $maxrows);
-        // echo "<b> $cat -> $where : " . count($groups) . " results</b><br />\n" ; //DEBUG
-        $res = array_merge($res, $groups);
-    }
-    return $res;
-}
-
-/**
- * search groups according to filters
- * @global type $DB
- * @param string $token to search in cohort table
- * @param string $category Group/cohort category, see below
- * @param int $maxrows
- * @return array
- */
-function mws_search_groups_category($token, $category, $maxrows) {
-    global $DB;
-    $ptoken = '%' . $DB->sql_like_escape($token) . '%';
-
-    $wherecat = categoryToWhere();
-    $cterms = explode('|', $category);
-    $cwhere = array();
-    foreach ($cterms as $term) {
-        if (isset($wherecat[$term])) {
-            $cwhere[] = $wherecat[$term];
-        }
-    }
-    if (!$cwhere) {
-        return array();
-    }
-    $sql = "SELECT id, name, idnumber, description, descriptionformat FROM {cohort} WHERE "
-        . "( name LIKE ? OR idnumber LIKE ? ) AND (" . join(' OR ', $cwhere) . ')' ;
-    // echo $sql . " <br />\n" ; //DEBUG
-    $records = $DB->get_records_sql($sql, array($ptoken, $ptoken), 0, $maxrows);
-    $groups = array();
-    $order = 0;
-    foreach ($records as $record) {
-        $order++;
-        $size = $DB->count_records('cohort_members', array('cohortid' => $record->id));
-        $groups[] = array(
-            'key' => $record->idnumber,
-            'name' => $record->name,
-            'description' => format_text($record->description, $record->descriptionformat),
-            'category' => groupKeyToCategory($record->idnumber),
-            'size' => $size,
-            'order' => $order
-        );
-    }
-    return $groups;
-}
-
 
 /**
  * emulates wsgroups "userGroupsId" action from Moodle data
@@ -129,8 +197,6 @@ function mws_userGroupsId($uid) {
      }
     return $groups;
 }
-
-
 
 /**
  * function provided by Pascal Rigaux, cf http://tickets.silecs.info/mantis/view.php?id=1642 (5089)
@@ -183,29 +249,6 @@ function groupYearlyPredicate() {
     return $groupYearly;
 }
 
-
-/**
- * sort of reciprocal from groupKeyToCategory
- * return array assoc. array of WHERE conditions in the SQL syntax
- */
-function categoryToWhere() {
-    $patterns = array(
-        'structures' => 'structures-%',
-        'affiliation' => 'affiliation-%',
-        'diploma' => 'diploma-%',
-        'gpelp' => 'groups-gpelp.%',
-        //'gpetp' => 'groups-gpetp.%',
-        'elp' => 'groups-mati%'
-    );
-    $res = array();
-    $other = '';
-    foreach ($patterns as $cat => $pattern) {
-        $res[$cat] = "idnumber LIKE '$pattern' ";
-        $other = $other . "idnumber NOT LIKE '$pattern' AND ";
-    }
-    $res['other'] = substr($other, 0, -4); //drop the last AND
-    return $res;
-}
 
 /**
  * @param string $haystack
