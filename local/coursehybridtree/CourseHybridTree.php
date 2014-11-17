a94d830399f553d870da30703b5afee536c7f04a
Guillaume Allegre <guillaume.allegre@silecs.info>
2014-11-17 16:44:50 +0100

    CourseHybridTree uses the debug param/attr

--- local/coursehybridtree/CourseHybridTree.php
+++ local/coursehybridtree/CourseHybridTree.php
@@ -13,21 +13,24 @@ class CourseHybridTree
      * Return a new instance of one of the ChtNode*.
      *
      * @param string $node
+     * @param bool $debug whether we display debug information before the entry name
      * @return ChtNode
      */
-    static public function createTree($node) {
+    static public function createTree($node, $debug=false) {
         $m = array();
         if (preg_match('#/cat(\d+)$#', $node, $m)) {
             // root node, given through a category
-            return ChtNodeCategory::buildFromCategoryId($m[1]);
+            $node = ChtNodeCategory::buildFromCategoryId($m[1]);
         } else if (preg_match('/^{/', $node)) {
             // inside node (non-root), given through serialized attributes
             $attributes = json_decode($node);
             $class = $attributes->class;
             unset($attributes->class);
-            return $class::unserialize($attributes);
+            $node = $class::unserialize($attributes);
         } else {
             die('{label: "An error occured: wrong node request"}');
         }
+        $node->debug = $debug;
+        return $node;
     }
 }
