f538947c862d30023b2ba1aa2752ae9f7b756c16
Guillaume Allegre <guillaume.allegre@silecs.info>
2014-12-19 00:18:29 +0100

    coursehybridtree bugfix locallib.php

--- local/coursehybridtree/locallib.php
+++ local/coursehybridtree/locallib.php
@@ -8,8 +8,8 @@
  */
 
 require_once __DIR__ . '/../../config.php';
-require __DIR__ . '/CourseHybridTree.php';
-require __DIR__ . '/ChtNode.php';
-require __DIR__ . '/ChtNodeCategory.php';
-require __DIR__ . '/ChtNodeRof.php';
-require __DIR__ . '/ChtNodeCourse.php';
+require_once __DIR__ . '/CourseHybridTree.php';
+require_once __DIR__ . '/ChtNode.php';
+require_once __DIR__ . '/ChtNodeCategory.php';
+require_once __DIR__ . '/ChtNodeRof.php';
+require_once __DIR__ . '/ChtNodeCourse.php';
