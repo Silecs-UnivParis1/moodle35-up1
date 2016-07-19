8fc73779acf55eb0236be222002899b6d7b3d6ad
Guillaume Allegre <guillaume.allegre@silecs.info>
2016-07-19 16:39:36 +0200

    post 3.1 : mod_etherpad updated to Moodle 3.1 conventions

--- mod/etherpad/version.php
+++ mod/etherpad/version.php
@@ -1,8 +1,10 @@
 <?PHP // $Id: version.php,v 1.0 2012/03/28 12:30:00 Serafim Panov Exp $
 
-$module->version   = 2013041700;  // The current module version (Date: YYYYMMDDXX)
-$module->requires  = 2011120500;  
-$module->release = 'Version 2.3';
-$module->component = 'mod_etherpad';
-$module->maturity = MATURITY_STABLE;
-$module->cron      = 0;           // Period for cron to check this module (secs)
+$plugin->version   = 2013041701;  // The current module version (Date: YYYYMMDDXX)
+// updated by Silecs to accept Moodle 3.1 convention ($module replaced by $plugin)
+
+$plugin->requires  = 2011120500;  
+$plugin->release = 'Version 2.3';
+$plugin->component = 'mod_etherpad';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->cron      = 0;           // Period for cron to check this module (secs)
