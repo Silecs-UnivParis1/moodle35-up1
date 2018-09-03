Silecs-UnivParis1/moodle-up1

course_validated : fonction accessible menu admin course -> course to validate

lang/en/admin.php
@@ -363,6 +363,7 @@ $string['courserequests'] = 'Course requests';
 $string['courserequestspending'] = 'Pending course requests';
 $string['courses'] = 'Courses';
 $string['coursesperpage'] = 'Courses per page';
+$string['coursestovalidate'] = 'Course to validate';
 $string['creatornewroleid'] = 'Creators\' role in new courses';
 $string['creatornewroleid_help'] = 'If the user does not already have the permission to manage the new course, the user is automatically enrolled using this role.';
 $string['cron'] = 'Cron';

Silecs-UnivParis1/moodle35-up1

Conflicts:
	lang/en/admin.php

lang/en/admin.php
@@ -367,6 +367,7 @@ $string['courserequests'] = 'Course requests';
 $string['courserequestspending'] = 'Pending course requests';
 $string['courses'] = 'Courses';
 $string['coursesperpage'] = 'Courses per page';
+$string['coursestovalidate'] = 'Courses to validate';
 $string['courseswithsummarieslimit'] = 'Courses with summaries limit';
 $string['creatornewroleid'] = 'Creators\' role in new courses';
 $string['creatornewroleid_help'] = 'If the user does not already have the permission to manage the new course, the user is automatically enrolled using this role.';