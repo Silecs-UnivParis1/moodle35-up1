Silecs-UnivParis1/moodle-up1

M2310 update categories idnumber with CLI parameters.

local/roftools/cli/updatecat.php
@@ -3,11 +3,48 @@
 define('CLI_SCRIPT', true);
 require(dirname(dirname(dirname(__DIR__))).'/config.php'); // global moodle config file.
 require(dirname(__DIR__) . '/updatelib.php');
+require_once($CFG->libdir.'/clilib.php');      // cli only functions
 
-echo "updating course categories... \n";
+// now get cli options
+list($options, $unrecognized) = cli_get_params(
+    array('help'=>false, 'list-old'=>false, 'up-idnumber'=>false, 'year'=>'', 'etab'=>''),
+    array('h'=>'help')
+    );
 
-update_categories_idnumber();
+if ($unrecognized) {
+    $unrecognized = implode("\n  ", $unrecognized);
+    cli_error(get_string('cliunknowoption', 'admin', $unrecognized));
+}
 
-update_rof_settings();
+$help =
+"Update course_categories field idnumber
+Options:
+--up-idnumber         action update. Mandatory.
+  --year=...          year code, ex. '2012-2013'
+  --etab=...          etablissement code, ex. 'UP1'
+--list-old            list old idnumbers
+-h, --help            Print out this help
+
+";
+
+if ( ! empty($options['help']) ) {
+    echo $help;
+    return 0;
+}
+
+
+// Ensure errors are well explained
+$CFG->debug = DEBUG_NORMAL;
+
+
+if ( $options['up-idnumber'] ) {
+    update_categories_idnumber($options['year'], $option['etab']);
+    return 0;
+}
+
+if ( $options['list-old'] ) {
+    list_old_idnumber();
+    return 0;
+}
 
 echo "OK.\n";

Silecs-UnivParis1/moodle35-up1

local/roftools/cli/updatecat.php
@@ -3,11 +3,48 @@
 define('CLI_SCRIPT', true);
 require(dirname(dirname(dirname(__DIR__))).'/config.php'); // global moodle config file.
 require(dirname(__DIR__) . '/updatelib.php');
+require_once($CFG->libdir.'/clilib.php');      // cli only functions
 
-echo "updating course categories... \n";
+// now get cli options
+list($options, $unrecognized) = cli_get_params(
+    array('help'=>false, 'list-old'=>false, 'up-idnumber'=>false, 'year'=>'', 'etab'=>''),
+    array('h'=>'help')
+    );
 
-update_categories_idnumber();
+if ($unrecognized) {
+    $unrecognized = implode("\n  ", $unrecognized);
+    cli_error(get_string('cliunknowoption', 'admin', $unrecognized));
+}
 
-update_rof_settings();
+$help =
+"Update course_categories field idnumber
+Options:
+--up-idnumber         action update. Mandatory.
+  --year=...          year code, ex. '2012-2013'
+  --etab=...          etablissement code, ex. 'UP1'
+--list-old            action list old idnumbers
+-h, --help            Print out this help
 
-echo "OK.\n";
+";
+
+if ( ! empty($options['help']) ) {
+    echo $help;
+    return 0;
+}
+
+
+// Ensure errors are well explained
+$CFG->debug = DEBUG_NORMAL;
+
+
+if ( $options['up-idnumber'] ) {
+    update_categories_idnumber($options['year'], $options['etab']);
+    return 0;
+}
+
+if ( $options['list-old'] ) {
+    list_old_idnumber();
+    return 0;
+}
+
+echo "Aucune action indiquée.\n";