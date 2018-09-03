Silecs-UnivParis1/moodle-up1

up1stats + topcohorts independant page

report/up1stats/topcohorts.php
@@ -1,0 +1,36 @@
+<?php
+
+/**
+ * UP1 Users Statistics
+ *
+ * @package    report
+ * @subpackage up1stats
+ * @copyright  2012 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('NO_OUTPUT_BUFFERING', true);
+
+require('../../config.php');
+require_once($CFG->dirroot.'/report/up1stats/locallib.php');
+require_once($CFG->libdir.'/adminlib.php');
+
+require_login();
+
+$howmany = optional_param('number', 50, PARAM_INT);
+
+// Print the header.
+admin_externalpage_setup('reportup1stats', '', null, '', array('pagelayout'=>'report'));
+echo $OUTPUT->header();
+
+echo $OUTPUT->heading('Cohorts - top '.$howmany);
+
+$url = "$CFG->wwwroot/report/up1stats/index.php";
+
+echo "<h3>Cohortes - top $howmany</h3>\n";
+$table = new html_table();
+$table->head = array('Effectif', 'Nom', 'Id');
+$table->data = report_up1stats_cohorts_top($howmany);
+echo html_writer::table($table);
+
+echo $OUTPUT->footer();

Silecs-UnivParis1/moodle35-up1

report/up1stats/topcohorts.php
@@ -1,0 +1,36 @@
+<?php
+
+/**
+ * UP1 Users Statistics - Top NN cohorts page
+ *
+ * @package    report
+ * @subpackage up1stats
+ * @copyright  2012 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('NO_OUTPUT_BUFFERING', true);
+
+require('../../config.php');
+require_once($CFG->dirroot.'/report/up1stats/locallib.php');
+require_once($CFG->libdir.'/adminlib.php');
+
+require_login();
+
+$howmany = optional_param('number', 50, PARAM_INT);
+
+// Print the header.
+admin_externalpage_setup('reportup1stats', '', null, '', array('pagelayout'=>'report'));
+echo $OUTPUT->header();
+
+echo $OUTPUT->heading('Cohortes - top '.$howmany);
+
+$url = "$CFG->wwwroot/report/up1stats/index.php";
+
+echo "<h3>Cohortes - top $howmany</h3>\n";
+$table = new html_table();
+$table->head = array('Effectif', 'Nom', 'Id');
+$table->data = report_up1stats_cohorts_top($howmany);
+echo html_writer::table($table);
+
+echo $OUTPUT->footer();