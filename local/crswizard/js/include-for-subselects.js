911f180bb6461837f08c79d05fd11a3de7265bdb
Guilloux <cecile.guilloux@silecs.info>
2019-03-07 11:53:04 +0100

    M#4136 crswizard hors ROF - subselects category - js

--- local/crswizard/js/include-for-subselects.js
+++ local/crswizard/js/include-for-subselects.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
         labels: ["Période :", "Établissement :", "Composante :", "Type de diplôme :"]
     });
 
+    /**
     $('#fitem_id_category > div.subselects > div:first-child').change(
         function() {
             var etab = $('#fitem_id_category > div.subselects > div:nth-child(2) > div.felement > select');
-            if (etab.children().length < 3) {
+            if (etab.children().length == 2) {
                 var paris = etab.children('option').eq(1);
                 etab.val(paris.val()).trigger('change');
             }
         }
-    );
+    );**/
 });
