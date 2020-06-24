ae0f8b739cb8f3f747b07e5422bc5fddbdc90da4
Guilloux <cecile.guilloux@silecs.info>
2020-06-24 16:34:30 +0200

    suppression plugin local_coursehybridtree

--- local/coursehybridtree/tests/all.php
+++
@@ -1,8 +0,0 @@
-<?php
-$files = glob(__DIR__ . '/*Test.php');
-
-foreach ($files as $f) {
-	$name = preg_replace('/Test.php$/', '', basename($f));
-	echo "\n$name\n" . str_repeat("=", strlen($name)) . "\n";
-	require $f;
-}
